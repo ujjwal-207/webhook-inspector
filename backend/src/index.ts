599537e7ddd5526bf8e32bf18e03e0f45ffeb412
ujjwal-207 <ujjwalnepal2060@gmail.com>
2025-09-01 19:07:10 +0545

    database connected in backend

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./utils/db";
+dotenv.config();
+connectDB();
+const port = 5000;
+
+const app = express();
+
+app.use(express.json());
+
+const corsOptions ={
+  origin:"http://localhost:5173/",
+  credentials:true,
+};
+app.use(cors(corsOptions));
+
+
+app.listen(port,()=>{
+  console.log('Server is running in port 5000');
+})
+
+
+
